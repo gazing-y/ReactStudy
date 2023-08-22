b7200305d5b65e6f18778ab79e9e72a38f37486a
yuanliping <yuanliping@tenorshare.cn>
2023-08-22 12:18:30 +0800

    project

--- 脚手架自带文件/src/reportWebVitals.js
+++ 脚手架自带文件/src/reportWebVitals.js
@@ -1,3 +1,4 @@
+// 页面性能
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
